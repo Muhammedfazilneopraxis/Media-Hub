9fe34fb1fc98ac63d5e2d6d3f517775570605325
fazilfazil1 <fazilfazil1>
2022-11-07 18:29:02 +0530

    social login ui created

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -8,9 +8,21 @@ const Body = ({ children }: any) => {
             {/* <HeadSection></HeadSection> */}
             {/* <hr></hr> */}
             {/* <div className='bg-red-500 w-1/2 h-1/2 overflow-hidden'> */}
-                {/* <p>This is body</p> */}
-                <img src={img}  className='h-full aspect-video m-auto overflow-hidden object-cover' />
-
+            {/* <p>This is body</p> */}
+            {/* <img src={img}  className='h-full aspect-video m-auto overflow-hidden object-cover' /> */}
+            <div className="grid grid-cols-2  h-full grid-flow-row-dense  grid-cols-3">
+                <div className='bg-white col-span-2'>
+                    <div className='container text-center flex h-screen flex-col  justify-center items-center '>
+                        <h1 className='font-bold text-2xl'>Media Hub</h1>
+                        <p>Bussiness can share medias with the bussiness customer and customer can share medias to the bussiness</p>
+                    </div>
+                </div>
+                <div className='bg-slate-200 flex flex-col justify-center'>
+                    <button className="bg-red-700 mx-7 text-white font-bold py-2 px-4 rounded-full ">
+                        Sign in with Google
+                    </button>
+                </div>
+            </div>
             {/* </div> */}
         </div>
     )
