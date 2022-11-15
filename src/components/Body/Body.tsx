1285a936cc6210e71332f4456b4695995693e4eb
fazilfazil1 <fazilfazil1>
2022-11-15 18:12:14 +0530

    btn class added

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -3,13 +3,16 @@ import s from './Body.module.css'
 import img from '../../images/commingsoon.png'
 import Pages from '../Pages/Createpage'
 import { signInWithGoogle } from '../../Firebase'
+import Center from '../Common/Center'
 const Body = ({ children }: any) => {
     return (
         <div className={s.rootContainer}>
             <HeadSection></HeadSection>
             <hr></hr>
             {/* <Pages /> */}
-            <button onClick={signInWithGoogle}>Sign in with google</button>
+            <Center>
+                <button onClick={signInWithGoogle} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Sign in with google</button>
+            </Center>
             <h1>{localStorage.getItem("name")}</h1>
             <h2>{localStorage.getItem("email")}</h2>
             <img src={localStorage.getItem("profilepic") ?? ""} />
