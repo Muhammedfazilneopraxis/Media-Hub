888e837353633a241c56ea529c9e140f584ea686
fazilfazil1 <fazilfazil1>
2022-11-15 18:19:27 +0530

    added bussinesslogin.tsx file

---
+++ src/components/SocialButton/GoogleLoginButton.tsx
@@ -0,0 +1,12 @@
+// refered for google login https://blog.logrocket.com/guide-adding-google-login-react-app/ 
+
+
+function GoogleButton() {
+  return (
+    <div>
+        <button>sign in google</button>
+    </div>
+  );
+}
+
+export default GoogleButton;
---
+++ src/components/SocialLogin/BussinessLogin.tsx
@@ -0,0 +1,21 @@
+import GoogleButton from '../SocialButton/GoogleLoginButton';
+import Center from '../Common/Center';
+
+
+const BussinessLogin = () => {
+    return (
+        <div className="grid grid-cols-2  h-full grid-cols-3">
+            <div className='bg-white col-span-2'>
+                <div className='text-center flex h-screen flex-col  justify-center items-center '>
+                    <h1 className='font-bold text-2xl'>Media Hub</h1>
+                    <p>Bussiness can share medias with the bussiness customer and customer can share medias to the bussiness</p>
+                </div>
+            </div>
+            <Center className='bg-slate-200 flex flex-col'>
+                    <GoogleButton />
+            </Center>
+        </div>
+    )
+}
+
+export default BussinessLogin;
